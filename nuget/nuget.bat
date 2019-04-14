9e7d6ca713ec6d849bfced5042e17e17a3a79718
Dale Newman <dalenewman@gmail.com>
2019-04-14 17:29:05 -0400

    updated to 0.5 dependencies

--- nuget/nuget.bat
+++ nuget/nuget.bat
@@ -1,8 +1,8 @@
 nuget pack Transformalize.Transform.GoogleMaps.nuspec -OutputDirectory "c:\temp\modules"
 nuget pack Transformalize.Transform.GoogleMaps.Autofac.nuspec -OutputDirectory "c:\temp\modules"
 
-nuget push "c:\temp\modules\Transformalize.Transform.GoogleMaps.0.3.12-beta.nupkg" -source https://api.nuget.org/v3/index.json
-nuget push "c:\temp\modules\Transformalize.Transform.GoogleMaps.Autofac.0.3.12-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Transform.GoogleMaps.0.5.0-beta.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget push "c:\temp\modules\Transformalize.Transform.GoogleMaps.Autofac.0.5.0-beta.nupkg" -source https://api.nuget.org/v3/index.json
 
 
 
